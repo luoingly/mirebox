4df206c3063534b6447f28efa323e5b48438ba25
luoingly <signature@luoingly.top>
2026-08-25 21:02:13 +0800

    Initial Commit

---
+++ Mirebox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mirebox
+//
+//  Created by 洛樱 on 2026/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mirebox/MireboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MireboxApp.swift
+//  Mirebox
+//
+//  Created by 洛樱 on 2026/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MireboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
